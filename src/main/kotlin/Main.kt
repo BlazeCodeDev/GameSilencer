dae457794b1fba777d5b410d929476371fc10f7f
BlazeCodeDev <blazecodeapps@gmail.com>
2022-09-27 14:52:11 +0200

    Update UI

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,23 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.toUpperCase
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.MenuBar
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import enums.Status
+import kotlin.system.exitProcess
 
 var isEnabled = mutableStateOf(false)
 var sliderValue = mutableStateOf(0)
@@ -27,32 +36,55 @@ fun App() {
     val timer = Timer
 
         MaterialTheme {
-            Column {
+            Column (modifier = Modifier
+                .background(Color(0xFF1e1b1e))
+                .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
                 Row(verticalAlignment = Alignment.CenterVertically) {
-                    Text("isActive")
+                    Text("Service Active", color = Color(0xFFe8e0e5))
                     Switch(checked = isEnabled.value,
-                        onCheckedChange = { isEnabled.value = it })
+                        onCheckedChange = { isEnabled.value = it },
+                        colors = SwitchDefaults.colors(
+                            checkedTrackColor = Color(0xFFf7d9ff),
+                            checkedThumbColor = Color(0xFFffffff),
+                            uncheckedTrackColor = Color(0xFF7d4996)
+                        ))
 
-                    Text(newStatus)
+                    Text(newStatus, color = Color(0xFFe8e0e5))
                 }
-                Text(sliderPosition.toString().split(".")[0])
-                Text(newTime)
-                Row {
 
-                    Slider(
-                        value = sliderPosition,
-                        onValueChange = { sliderPosition = it; sliderValue.value = sliderPosition.toInt()},
-                        valueRange = 1F..45F,
-                        steps = 8)
+                Text(newTime)
+                Card (backgroundColor = Color(0xFF422c42),
+                    shape = MaterialTheme.shapes.small,
+                    modifier = Modifier.padding(8.dp)){
+                    Row (horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically){
+                        Text(sliderPosition.toString().split(".")[0] + "s",
+                            color = Color(0xFFe8e0e5),
+                            modifier = Modifier.padding(8.dp))
+                        Slider(
+                            value = sliderPosition,
+                            onValueChange = { sliderPosition = it; sliderValue.value = sliderPosition.toInt()},
+                            valueRange = 1F..45F,
+                            steps = 8,
+                            colors = SliderDefaults.colors(
+                                thumbColor = Color(0xFFffffff),
+                                activeTrackColor = Color(0xFFf7d9ff),
+                                inactiveTrackColor = Color(0xFF310048)
+                            )
+                        )
+                    }
                 }
             }
-
         }
     }
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
+        state = rememberWindowState(width = 500.dp, height = 300.dp),
+        title = "GameSilencer",
     ) {
         val keyListener = KeyListener()
         keyListener.init()
