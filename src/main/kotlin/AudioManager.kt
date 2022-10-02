e5b45a368d8035af156d0ba8aacee3a8dd50c79f
BlazeCodeDev <blazecodeapps@gmail.com>
2022-10-02 14:46:44 +0200

    Fix keyevent

--- src/main/kotlin/AudioManager.kt
+++ src/main/kotlin/AudioManager.kt
@@ -1,8 +1,9 @@
 import com.github.kwhat.jnativehook.GlobalScreen
 import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent
+import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent.VC_MEDIA_PLAY
 
 class AudioManager {
     fun playPause(){
-        GlobalScreen.postNativeEvent(NativeKeyEvent(2401, 0, 179, 57378, NativeKeyEvent.CHAR_UNDEFINED))
+        GlobalScreen.postNativeEvent(NativeKeyEvent(2401, 0, 179, VC_MEDIA_PLAY , NativeKeyEvent.CHAR_UNDEFINED))
     }
 }
